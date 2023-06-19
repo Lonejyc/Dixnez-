d551d65464195b6966a9b5e5a4b092d9b411092e
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2023-06-19 17:06:17 +0200

    Update 1.2
    Changement of the 17/06/2023

--- js/display.js
+++ js/display.js
@@ -4,6 +4,7 @@ document.querySelectorAll('.object').forEach((objectEl) => {
     const empruntEl = objectEl.querySelector(".emprunt")
     const renduEl = objectEl.querySelector(".rendu")
     const userFormEl = objectEl.querySelector('.user_form');
+    const formulaireEl = objectEl.querySelector('.formulaire');
     const formEl = objectEl.querySelector('form');
     
     btnElements.forEach((btnEl) => {
@@ -11,23 +12,27 @@ document.querySelectorAll('.object').forEach((objectEl) => {
             
             if(btnEl.classList.contains('emprunter_btn')) {
                 empruntEl.style.display = "flex";
+                formulaireEl.style.display = "flex";
             }
 
             if(btnEl.classList.contains('emprunterClose')) {
                 empruntEl.style.display = "none";
                 userFormEl.style.display = 'none';
+                formulaireEl.style.display = "none";
                 formEl.reset();
             }
 
 
             if(btnEl.classList.contains('rendre_btn')) {
                 renduEl.style.display = "flex";
+                formulaireEl.style.display = "flex";
             }
 
            
             if(btnEl.classList.contains('rendreClose')) {
                 renduEl.style.display = "none";
                 userFormEl.style.display = 'none';
+                formulaireEl.style.display = "none";
                 formEl.reset();
             }
         })
